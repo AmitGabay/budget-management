928f359ab9e7c726b66f9b26d76d6abba2be9ffb
Amit <amit.b.gabay@gmail.com>
2022-08-03 16:14:49 +0300

    add styling to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+import style from "./Navbar.module.css";
+
 const Navbar = () => {
-  return <h1>navbar</h1>;
+  const activeClassName = style.selected;
+
+  return (
+    <div className={style.navbar}>
+      <p>Budget Managment</p>
+      <NavLink
+        to={"/"}
+        className={`${({ isActive }) => isActive && activeClassName} ${
+          style.text
+        }`}
+      >
+        Daily
+      </NavLink>
+      <NavLink to={"/month"} className={style.text}>
+        Monthly
+      </NavLink>
+    </div>
+  );
 };
 
 export default Navbar;
