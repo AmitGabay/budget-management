92af77aadb27b4ed623812a37c16a57be53c1f91
Amit <amit.b.gabay@gmail.com>
2022-10-27 15:43:07 +0300

    add basic table and current date to Daily page

--- src/pages/Daily/Daily.jsx
+++ src/pages/Daily/Daily.jsx
@@ -1,5 +1,40 @@
+import React from "react";
+import Table from "../../components/Table/Table";
+
 const Daily = () => {
-  return <h1>Hello Day</h1>;
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
+  const columns = [
+    {
+      Header: "Sum",
+      accessor: "sum",
+    },
+    {
+      Header: "Card",
+      accessor: "card",
+    },
+    {
+      Header: "Category",
+      accessor: "category",
+    },
+  ];
+
+  const data = [
+    {
+      sum: 500,
+      card: "hever",
+      category: "food",
+    },
+  ];
+  return (
+    <div>
+      <h2>Summary of expenses</h2>
+      <h3>{date}</h3>
+      <Table columns={columns} data={data} />
+    </div>
+  );
 };
 
 export default Daily;
